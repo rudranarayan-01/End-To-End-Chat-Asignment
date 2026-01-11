5d1fcf0b56b9512df9f767111107b897a959c4c0
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-01-11 22:51:22 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,12 +14,12 @@ bcrypt = Bcrypt(app)
 
 # Deployment changes
 
-CORS(app, resources={r"/*": {"origins": "https://chat-end-to-end-frontend.onrender.com"}})
+CORS(app, resources={r"/*": {"origins": "https://chat-end-toend-frontend.onrender.com"}})
 
 # 2. Update SocketIO for real-time WebSocket connections
 socketio = SocketIO(
     app, 
-    cors_allowed_origins="https://chat-end-to-end-frontend.onrender.com",
+    cors_allowed_origins="https://chat-end-toend-frontend.onrender.com",
     async_mode='gevent' # As established for your Render deployment
 )
 
